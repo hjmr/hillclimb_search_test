ddb2c0054795cf2e0a9ea234eb577b357c05cd9b
hjmr <2620990+hjmr@users.noreply.github.com>
2022-12-09 15:19:06 +0900

    change random.standard_normal to random.uniform

--- hcsearch_test.py
+++ hcsearch_test.py
@@ -5,11 +5,11 @@ from simpleai.search.local import hill_climbing, simulated_annealing
 from simpleai.search.viewers import ConsoleViewer
 from gensim.models import KeyedVectors
 
-NUM_CANDIDATES = 5  # 一度に探索する単語の数
+NUM_CANDIDATES = 10  # 一度に探索する単語の数
 
 vectors = KeyedVectors.load("../chive-1.2-mc30_gensim/chive-1.2-mc30.kv")
 
-target = "python"
+target = "人工知能"
 target_vector = vectors[target]
 
 
@@ -20,8 +20,8 @@ class QuerySearchProblem(SearchProblem):
         actions = []
         curr_vector = vectors[curr_query]
         for _ in range(NUM_CANDIDATES):
-            new_vector = curr_vector + np.random.standard_normal(curr_vector.size)
-            new_keywords = vectors.similar_by_vector(new_vector)
+            new_vector = curr_vector + np.random.uniform(size=curr_vector.size, low=-0.5, high=0.5)
+            new_keywords = vectors.similar_by_vector(new_vector, topn=NUM_CANDIDATES)
             for k, _ in new_keywords:
                 # 現在のqueryと異なり，かつすでにクエリ候補に入っていない単語をクエリ候補とする
                 if k != curr_query and k not in actions:
@@ -44,7 +44,7 @@ class QuerySearchProblem(SearchProblem):
         return v
 
 
-initial_query = "Java"
+initial_query = "python"
 problem = QuerySearchProblem(initial_state=initial_query)
 result = simulated_annealing(problem, iterations_limit=100, viewer=ConsoleViewer())
 # result = hill_climbing(problem, iterations_limit=100, viewer=ConsoleViewer())

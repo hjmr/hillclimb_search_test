44af59b28d9cac4aa20d840efff6064a61964749
hjmr <2620990+hjmr@users.noreply.github.com>
2022-12-09 14:15:35 +0900

    use simulated_annealing.

--- hcsearch_test.py
+++ hcsearch_test.py
@@ -1,58 +1,50 @@
 import numpy as np
 import random
-from simpleai.search import SearchProblem, hill_climbing
+from simpleai.search import SearchProblem
+from simpleai.search.local import hill_climbing, simulated_annealing
+from simpleai.search.viewers import ConsoleViewer
 from gensim.models import KeyedVectors
 
-NUM_CANDIDATES = 5 # 一度に探索する単語の数
-NUM_MOD_RATIO = 0.33 # 33%の要素を変更
-TARGET = "Java"
+NUM_CANDIDATES = 5  # 一度に探索する単語の数
 
 vectors = KeyedVectors.load("../chive-1.2-mc5_gensim/chive-1.2-mc5.kv")
-# ↓ 必ずしも TARGET に設定した単語が含まれているとは限らないので
-target_vector = vectors[vectors.similar_by_key(TARGET)[0][0]]
+
+target = "python"
+target_vector = vectors[target]
+
 
 class QuerySearchProblem(SearchProblem):
-    def actions(self, curr_state):
-        num_mod_indices = int(len(curr_state) * NUM_MOD_RATIO)
+    # 次の状態に至るための action のリストを生成
+    # ここでは次のクエリ候補を生成している
+    def actions(self, curr_query):
         actions = []
+        curr_vector = vectors[curr_query]
         for _ in range(NUM_CANDIDATES):
-            idx_candidates = list(range(len(curr_state)))
-            pos_dif = []
-            for _ in range(num_mod_indices):
-                rnd = random.randrange(len(idx_candidates))
-                pos = idx_candidates.pop(rnd)
-                dif = random.random() * 1.0 - 0.5 # -0.5〜0.5の範囲で変更
-                pos_dif.append((pos, dif))
-            actions.append(pos_dif)
+            new_vector = curr_vector + np.random.standard_normal(curr_vector.size)
+            new_keywords = vectors.similar_by_vector(new_vector)
+            for k, _ in new_keywords:
+                # 現在の query と異なる単語を候補にする
+                if k != curr_query:
+                    actions.append(k)
+                    break
         return actions
 
-    def value(self, curr_state):
-        keywords = vectors.similar_by_vector(curr_state, topn=5)
-        print(f"keywords = {keywords}")
-        keyword = keywords[0][0] 
-        keyword_vector = vectors[keyword]
-        dvec = keyword_vector - target_vector
-        d = 1.0 / (1.0 + np.linalg.norm(dvec)) + random.random() * 0.1
-        print(f"value = {d}")
-        return d
+    # 状態にアクションを適用した際の次の状態を生成
+    # アクション＝次のクエリ候補なのでそのまま戻す
+    def result(self, curr_query, action):
+        return action
 
-    def result(self, curr_state, action):
-        new_state = np.array(curr_state)
-        # action = list[(pos, dif)]
-        # pos: ベクトル上の位置（要素番号）
-        # dif: ベクトルの要素に加算する乱数の値
-        for p, d in action:
-            new_state[p] += d
-        return new_state
-
-    def is_goal(self, curr_state):
-        keyword = vectors.similar_by_vector(curr_state)[0][0]
-        return keyword == TARGET
+    # 状態の価値を計算
+    # ここではベクトルの差（ノルム）の逆数を価値としている
+    def value(self, curr_query):
+        curr_vector = vectors[curr_query]
+        d = curr_vector - target_vector
+        v = 1.0 / (1.0 + np.linalg.norm(d))
+        return v
 
 
-initial_query = "python"
-initial_query_vector = vectors[vectors.similar_by_word(initial_query)[0][0]]
-problem = QuerySearchProblem(initial_state = initial_query_vector)
-result = hill_climbing(problem)
+initial_query = "Java"
+problem = QuerySearchProblem(initial_state=initial_query)
+result = simulated_annealing(problem, iterations_limit=100, viewer=ConsoleViewer())
 
 print(result.path())

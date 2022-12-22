ba94cffa694d72662739c757a6598f917a475589
hjmr <2620990+hjmr@users.noreply.github.com>
2022-12-23 00:32:22 +0900

    add genetic algorithm

---
+++ gasearch_test.py
@@ -0,0 +1,65 @@
+import numpy as np
+import random
+from simpleai.search import SearchProblem
+from simpleai.search.local import genetic
+from simpleai.search.viewers import ConsoleViewer
+from gensim.models import KeyedVectors
+
+vectors: KeyedVectors = KeyedVectors.load("../chive-1.2-mc30_gensim/chive-1.2-mc30.kv")
+
+start = "若手"
+start_vector = vectors[start]
+
+target = "クリスマス"
+target_vector = vectors[target]
+
+
+class QuerySearchProblem(SearchProblem):
+    def generate_random_state(self):
+        new_vec = start_vector + np.random.standard_normal(size=start_vector.size)
+        candidates = vectors.similar_by_vector(new_vec, topn=100)
+        return candidates[-1][0]
+
+    def crossover(self, query1, query2):
+        vec1 = vectors[query1]
+        vec2 = vectors[query2]
+        new_vec = (vec1 + vec2) / 2
+        candidates = vectors.similar_by_vector(new_vec, topn=10)
+        new_query = None
+        for q, _ in candidates:
+            if q != query1 and q != query2:
+                new_query = q
+                break
+        return new_query
+
+    def mutate(self, query):
+        vec = vectors[query]
+        new_vec = vec + np.random.standard_normal(size=vec.size)
+        candidates = vectors.similar_by_vector(new_vec, topn=10)
+        new_query = None
+        for q, _ in candidates:
+            if q != query:
+                new_query = q
+                break
+
+        return new_query
+
+    # 状態の価値を計算
+    # ここではベクトルの差（ノルム）の逆数を価値としている
+    def value(self, curr_query):
+        v = 0
+        try:
+            curr_vector = vectors[curr_query]
+            d = curr_vector - target_vector
+            v = 100.0 / (1.0 + np.linalg.norm(d))
+        except Exception as e:
+            print(e)
+            print(curr_query)
+        return v
+
+
+problem = QuerySearchProblem()
+# result = simulated_annealing(problem, iterations_limit=100, viewer=ConsoleViewer())
+result = genetic(problem, population_size=100, mutation_chance=0.1, iterations_limit=100, viewer=ConsoleViewer())
+
+print(result.state, result.path())
